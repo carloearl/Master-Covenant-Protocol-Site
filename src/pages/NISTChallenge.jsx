643bfb497a413703fb73f0cc727bd22df95a281f
Carlo Earl <carloearl@glyphlock.com>
2025-12-11 04:45:49 +0000

    File changes

--- src/pages/NISTChallenge.jsx
+++ src/pages/NISTChallenge.jsx
@@ -6,7 +6,6 @@
 import React from 'react';
 import SEOHead from '@/components/SEOHead';
 import Hero from '@/components/nist/Hero';
-import GlyphLockDifferenceSection from '@/components/marketing/GlyphLockDifferenceSection';
 import ModalityTabs from '@/components/nist/ModalityTabs';
 import CovenantDiagram from '@/components/nist/CovenantDiagram';
 import Timeline from '@/components/nist/Timeline';
@@ -37,9 +36,6 @@ export default function NISTChallengePage() {
 
       {/* Hero Section */}
       <Hero />
-
-      {/* What Makes GlyphLock Different */}
-      <GlyphLockDifferenceSection />
 
       {/* Overview Section */}
       <section className="py-16 bg-white">
